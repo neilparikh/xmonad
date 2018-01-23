51ae2bee0407e00dbbf7db3569ced1df20e47523
Neil Parikh <parikh.neil@me.com>
2018-01-23 18:00:47 -0500

    initial commit

---
+++ xmonad.hs
@@ -0,0 +1,21 @@
+import XMonad
+import XMonad.Util.CustomKeys
+import XMonad.Layout.Tabbed
+import XMonad.Layout.NoFrillsDecoration
+import XMonad.Hooks.DynamicLog
+
+myLayout = noFrillsDeco shrinkText def (layoutHook def) ||| simpleTabbed
+
+main = do
+  let conf = def {
+      borderWidth = 2
+    , layoutHook  = myLayout
+    , keys        = customKeys delkeys inskeys
+    -- , modMask     = mod4Mask
+  }
+  config <- xmobar conf
+  xmonad config
+  where
+  delkeys XConfig {modMask = modm} = []
+  inskeys conf@(XConfig {modMask = modm}) =
+      [((modm .|. controlMask, xK_Delete), spawn "light-locker-command --lock")]
